4fe8e07156f5b28210c71de90bf3ffba2a504fae
Steve Jung <jsuch2362@gmail.com>
2020-12-01 22:56:36 +0900

    remove redundant code

--- _sample/sample-app/src/main/java/com/declarative/ui/pattern/viewcomponents/body/child/BodyChildViewComponent.kt
+++ _sample/sample-app/src/main/java/com/declarative/ui/pattern/viewcomponents/body/child/BodyChildViewComponent.kt
@@ -1,11 +1,9 @@
 package com.declarative.ui.pattern.viewcomponents.body.child
 
 import com.declarative.ui.viewcomponent.ViewComponent
-import com.declarative.ui.viewcomponent.ViewComponentController
 import com.declarative.ui.viewcomponent.ViewComponentHolder
 
 class BodyChildViewComponent(viewModel: BodyChildViewModel,
                              viewRender: BodyChildViewRender) : ViewComponent(viewRender = viewRender, viewModel = viewModel)
 
-class BodyChildViewComponentHolder(bodyViewComponent: BodyChildViewComponent) : ViewComponentHolder(bodyViewComponent,
-                                                                                                    ViewComponentController.EMPTY)
+class BodyChildViewComponentHolder(bodyViewComponent: BodyChildViewComponent) : ViewComponentHolder(bodyViewComponent)
